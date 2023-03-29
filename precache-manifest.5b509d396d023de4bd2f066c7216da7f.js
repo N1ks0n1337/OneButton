611d68b53983ee5a731022aead9ad8a5cfd0fc35
N1ks0n1337 <92136787+N1ks0n1337@users.noreply.github.com>
2023-03-29 22:22:47 +0500

    build

---
+++ precache-manifest.5b509d396d023de4bd2f066c7216da7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0e9087450dcd5c7e444",
+    "url": "/static/css/main.70e25b27.chunk.css"
+  },
+  {
+    "revision": "c0e9087450dcd5c7e444",
+    "url": "/static/js/main.c0e90874.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "288be7b24413fee0738e",
+    "url": "/static/js/2.288be7b2.chunk.js"
+  },
+  {
+    "revision": "ba446cd038288152dfb4",
+    "url": "/static/js/runtime~main.ba446cd0.js"
+  },
+  {
+    "revision": "0c39616272308802deded9a8b76a420a",
+    "url": "/static/media/clickbait.0c396162.mp4"
+  },
+  {
+    "revision": "cc41efc2e191aa74b8bb68c5a717fa21",
+    "url": "/static/media/clickbait.cc41efc2.mp3"
+  },
+  {
+    "revision": "9c8298351388dd3cf3c12aed8f2493a0",
+    "url": "/index.html"
+  }
+];
